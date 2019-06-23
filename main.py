8807aca9ef0b40aa9681471fd9ca9560b34e26e2
Gina Leuty <ginaleuty15@gmail.com>
2019-06-23 13:05:17 -0500

    began function and form outlines in main and index

--- main.py
+++ main.py
@@ -5,3 +5,33 @@ app = Flask(__name__)
 
 app.config['DEBUG'] = True
 
+@app.route("/index.html")
+def filled_form():
+    name = request.form['name']
+
+    if (not name) or (name.strip() == ''):
+        error = "Please type in your name"
+        return redirect('/?error=' + error)
+
+    password = request.form['password']
+
+    if (not password) or (password.strip() == ''):
+        error = "Please type in a valid password (3-20 characters long, no spaces)"
+        return redirect('/?error=' + error)
+
+    verify-password = request.form['verify_password']
+
+    if (not verify-password) or (verify-password.strip() == ''):
+        error = "Passwords do not match."
+        return redirect('/?error=' + error)
+    
+    e-mail = request.form['email']
+
+    # create conditionals for errors
+
+
+    return render_template("welcome.html", name=name)
+
+
+
+@app.route("/welcome.html")
